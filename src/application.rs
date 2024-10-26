a71070ccfb3a150cdf5469d9455dd8c0fa0b0048
Jannis Morgenstern <jannis.n.morgenstern@gmail.com>
2024-10-26 15:32:55 +0200

    feat: text rendering

--- src/application.rs
+++ src/application.rs
@@ -1,5 +1,10 @@
 use std::sync::Arc;
 
+use glyphon::{Color, Resolution, TextArea, TextBounds};
+use wgpu::{
+    CommandEncoderDescriptor, LoadOp, Operations, RenderPassColorAttachment, RenderPassDescriptor,
+    TextureViewDescriptor,
+};
 use winit::{
     application::ApplicationHandler, dpi::LogicalSize, event::WindowEvent, window::Window,
 };
@@ -56,8 +61,13 @@ impl ApplicationHandler for Application {
             queue,
             surface,
             surface_config,
+            font_system,
+            swash_cache,
+            viewport,
+            atlas,
             window,
-            ..
+            text_renderer,
+            text_buffer,
         } = state;
 
         match event {
@@ -67,7 +77,68 @@ impl ApplicationHandler for Application {
                 surface.configure(&device, &surface_config);
                 window.request_redraw();
             }
-            WindowEvent::RedrawRequested => {}
+            WindowEvent::RedrawRequested => {
+                viewport.update(
+                    &queue,
+                    Resolution {
+                        width: surface_config.width,
+                        height: surface_config.height,
+                    },
+                );
+
+                text_renderer
+                    .prepare(
+                        device,
+                        queue,
+                        font_system,
+                        atlas,
+                        viewport,
+                        [TextArea {
+                            buffer: text_buffer,
+                            left: 10.0,
+                            top: 10.0,
+                            scale: 1.0,
+                            bounds: TextBounds {
+                                left: 0,
+                                top: 0,
+                                right: 600,
+                                bottom: 160,
+                            },
+                            default_color: Color::rgb(255, 255, 255),
+                            custom_glyphs: &[],
+                        }],
+                        swash_cache,
+                    )
+                    .unwrap();
+
+                let frame = surface.get_current_texture().unwrap();
+                let view = frame.texture.create_view(&TextureViewDescriptor::default());
+                let mut encoder =
+                    device.create_command_encoder(&CommandEncoderDescriptor { label: None });
+                {
+                    let mut pass = encoder.begin_render_pass(&RenderPassDescriptor {
+                        label: None,
+                        color_attachments: &[Some(RenderPassColorAttachment {
+                            view: &view,
+                            resolve_target: None,
+                            ops: Operations {
+                                load: LoadOp::Clear(wgpu::Color::BLACK),
+                                store: wgpu::StoreOp::Store,
+                            },
+                        })],
+                        depth_stencil_attachment: None,
+                        timestamp_writes: None,
+                        occlusion_query_set: None,
+                    });
+
+                    text_renderer.render(&atlas, &viewport, &mut pass).unwrap();
+                }
+
+                queue.submit(Some(encoder.finish()));
+                frame.present();
+
+                atlas.trim();
+            }
             _ => {}
         }
     }
--- src/window.rs
+++ src/window.rs
@@ -1,10 +1,12 @@
 use std::sync::Arc;
 
+use glyphon::{Family, FontSystem, Shaping, SwashCache, TextAtlas, TextRenderer, Viewport};
 use winit::window::Window;
 
 use wgpu::{
-    CompositeAlphaMode, Device, DeviceDescriptor, Instance, InstanceDescriptor, PresentMode, Queue,
-    RequestAdapterOptions, Surface, SurfaceConfiguration, TextureFormat, TextureUsages,
+    CompositeAlphaMode, Device, DeviceDescriptor, Instance, InstanceDescriptor, MultisampleState,
+    PresentMode, Queue, RequestAdapterOptions, Surface, SurfaceConfiguration, TextureFormat,
+    TextureUsages,
 };
 
 pub struct WindowState {
@@ -12,13 +14,24 @@ pub struct WindowState {
     pub queue: Queue,
     pub surface: Surface<'static>,
     pub surface_config: SurfaceConfiguration,
+
+    pub font_system: FontSystem,
+    pub swash_cache: SwashCache,
+    pub viewport: Viewport,
+    pub atlas: TextAtlas,
+    pub text_renderer: TextRenderer,
+    pub text_buffer: glyphon::Buffer,
+
+    // Make sure that the winit window is last in the struct so that
+    // it is dropped after the wgpu surface is dropped, otherwise the
+    // program may crash when closed. This is probably a bug in wgpu.
     pub window: Arc<Window>,
 }
 
 impl WindowState {
     pub async fn new(window: Arc<Window>) -> Self {
         let physical_size = window.inner_size();
-        // let scale_factor = window.scale_factor();
+        let scale_factor = window.scale_factor();
 
         let instance = Instance::new(InstanceDescriptor::default());
         let adapter = instance
@@ -45,14 +58,43 @@ impl WindowState {
             view_formats: vec![],
             desired_maximum_frame_latency: 2,
         };
-
         surface.configure(&device, &surface_config);
+
+        let mut font_system = FontSystem::new();
+        let swash_cache = SwashCache::new();
+        let cache = glyphon::Cache::new(&device);
+        let viewport = Viewport::new(&device, &cache);
+        let mut atlas = TextAtlas::new(&device, &queue, &cache, swapchain_format);
+        let text_renderer =
+            TextRenderer::new(&mut atlas, &device, MultisampleState::default(), None);
+        let mut text_buffer =
+            glyphon::Buffer::new(&mut font_system, glyphon::Metrics::new(30.0, 42.0));
+
+        let physical_width = (physical_size.width as f64 * scale_factor) as f32;
+        let physical_height = (physical_size.height as f64 * scale_factor) as f32;
+
+        text_buffer.set_size(
+            &mut font_system,
+            Some(physical_width),
+            Some(physical_height),
+        );
+        text_buffer.set_text(&mut font_system,
+            "Hello world! 👋\nThis is rendered with 🦅 glyphon 🦁\nThe text below should be partially clipped.\na b c d e f g h i j k l m n o p q r s t u v w x y z",
+            glyphon::Attrs::new().family(Family::SansSerif),
+            Shaping::Advanced);
+        text_buffer.shape_until_scroll(&mut font_system, false);
 
         Self {
             device,
             queue,
             surface,
             surface_config,
+            font_system,
+            swash_cache,
+            viewport,
+            atlas,
+            text_renderer,
+            text_buffer,
             window,
         }
     }
